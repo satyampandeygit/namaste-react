bd8c1c09ebd702bc525cbe2401f3b17b2c59e437
psatyam360 <psatyam360@gmail.com>
2023-03-17 00:26:59 +0530

    Instamart Page addedd

--- FoodVilla/src/components/Instamart.js
+++ FoodVilla/src/components/Instamart.js
@@ -1,8 +1,32 @@
+import { useState } from "react"
+
+const Section = ({ title, description }) => {
+    const [isVisible, setIsVisible] = useState(false);
+    return (
+        <div className="border border-black p-2 m-2">
+            <h1>{title}</h1>
+            <button onClick={() => { isVisible? setIsVisible(false): setIsVisible(true) }}>
+                { !isVisible ? 'Show' : 'Hide' }
+            </button>
+            <p>{isVisible && description}</p>
+        </div>
+    )
+}
+
 const Instamart = () => {
     return (
         <div>
-            <h1>Instamart</h1>
-            <h3>100s of components</h3>
+            <h1 className="text-3xl font-bold p-2 m-2">Instamart</h1>
+            
+            <Section 
+                title="About Instamart"
+                description="This is about section of instamart"
+            />
+
+            <Section
+                title="Items"
+                description="This is item section of instamart page"
+            />
         </div>
     )
 }
